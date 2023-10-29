d25cb51120a7ca0c6c16df899b38215d713403f2
bamboo <nmz89825@gmail.com>
2023-10-29 16:37:16 +0800

    feat: options content modules inserted.

--- src/pages/options/Options.tsx
+++ src/pages/options/Options.tsx
@@ -1,7 +1,11 @@
-import React, {Suspense, useState} from 'react';
-import '@pages/options/Options.css';
-import Tab from '@pages/options/Tab';
+import React, { useState } from 'react';
+import Tab from '@pages/options/components/Tab';
 import icon from '@assets/img/icon-48.png';
+import '@pages/options/Options.css';
+
+import Settings from './module/Settings';
+import History from './module/History';
+import Feedback from './module/Feedback';
 
 const tabs = [
   'settings',
@@ -23,6 +27,13 @@ const Options: React.FC = () => {
               <Tab key={tab} hash={tab} isActive={curTab === tab} onClick={() => { setTab(tab); }}>{tab}</Tab>
             ))}
           </div>
+        </div>
+      </div>
+      <div className='opts-content'>
+        <div className='opts-content-inner'>
+          {curTab === 'settings' && <Settings />}
+          {curTab === 'history' && <History />}
+          {curTab === 'feedback' && <Feedback />}
         </div>
       </div>
     </div>
