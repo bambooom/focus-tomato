636e97f67278b4fa36a98609daa47a99f4c581ec
bamboo <nmz89825@gmail.com>
2023-10-29 14:27:59 +0800

    wip: disable content scripts first.

--- manifest.ts
+++ manifest.ts
@@ -29,14 +29,14 @@ const manifest: chrome.runtime.ManifestV3 = {
   icons: {
     '128': 'icon-128.png',
   },
-  content_scripts: [
-    {
-      matches: ['http://*/*', 'https://*/*', '<all_urls>'],
-      js: ['src/pages/content/index.js'],
-      // KEY for cache invalidation
-      css: ['assets/css/contentStyle<KEY>.chunk.css'],
-    },
-  ],
+  // content_scripts: [
+  //   {
+  //     matches: ['http://*/*', 'https://*/*', '<all_urls>'],
+  //     js: ['src/pages/content/index.js'],
+  //     // KEY for cache invalidation
+  //     css: ['assets/css/contentStyle<KEY>.chunk.css'],
+  //   },
+  // ],
   // devtools_page: 'src/pages/devtools/index.html',
   web_accessible_resources: [
     {
