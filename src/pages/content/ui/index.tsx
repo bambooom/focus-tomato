86b42d1cf77d9617ff99171f55294d9ed52b30e4
bamboo <nmz89825@gmail.com>
2023-12-12 01:22:00 +0800

    deps: update deps and remove twind as template.

--- src/pages/content/ui/index.tsx
+++ src/pages/content/ui/index.tsx
@@ -1,7 +1,7 @@
 import { createRoot } from 'react-dom/client';
-import App from '@src/pages/content/components/Demo/app';
+import App from '@pages/content/ui/app';
 import refreshOnUpdate from 'virtual:reload-on-update-in-view';
-import { attachTwindStyle } from '@src/shared/style/twind';
+import injectedStyle from './injected.css?inline';
 
 refreshOnUpdate('pages/content');
 
@@ -16,12 +16,16 @@ rootIntoShadow.id = 'shadow-root';
 const shadowRoot = root.attachShadow({ mode: 'open' });
 shadowRoot.appendChild(rootIntoShadow);
 
+/** Inject styles into shadow dom */
+const styleElement = document.createElement('style');
+styleElement.innerHTML = injectedStyle;
+shadowRoot.appendChild(styleElement);
+
 /**
  * https://github.com/Jonghakseo/chrome-extension-boilerplate-react-vite/pull/174
  *
  * In the firefox environment, the adoptedStyleSheets bug may prevent contentStyle from being applied properly.
  * Please refer to the PR link above and go back to the contentStyle.css implementation, or raise a PR if you have a better way to improve it.
  */
-attachTwindStyle(rootIntoShadow, shadowRoot);
 
 createRoot(rootIntoShadow).render(<App />);
--- src/pages/popup/Popup.tsx
+++ src/pages/popup/Popup.tsx
@@ -10,17 +10,27 @@ const Popup = () => {
   const theme = useStorage(exampleThemeStorage);
 
   return (
-    <div className="App">
-      <header className="App-header">
+    <div
+      className="App"
+      style={{
+        backgroundColor: theme === 'light' ? '#fff' : '#000',
+      }}>
+      <header className="App-header" style={{ color: theme === 'light' ? '#000' : '#fff' }}>
         <img src={logo} className="App-logo" alt="logo" />
-        <p className="text-lime-400">
+        <p>
           Edit <code>src/pages/popup/Popup.tsx</code> and save to reload.
         </p>
-        <a className="App-link" href="https://reactjs.org" target="_blank" rel="noopener noreferrer">
+        <a
+          className="App-link"
+          href="https://reactjs.org"
+          target="_blank"
+          rel="noopener noreferrer"
+          style={{ color: theme === 'light' && '#0281dc', marginBottom: '10px' }}>
           Learn React!
         </a>
         <button
           style={{
+            backgroundColor: theme === 'light' ? '#fff' : '#000',
             color: theme === 'light' ? '#fff' : '#000',
           }}
           onClick={exampleThemeStorage.toggle}>
